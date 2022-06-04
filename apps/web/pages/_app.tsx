0fafb027d3446e826ae9ca98a24845e6a0ac3b4d
aiden <recovery@asleep.ai>
2022-06-04 16:42:37 +0900

    set i18-next configuration

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,6 +1,7 @@
+import { appWithTranslation } from 'next-i18next';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
-import './styles.css';
+const i18config = require('../next-i18next.config');
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
@@ -15,4 +16,4 @@ function CustomApp({ Component, pageProps }: AppProps) {
   );
 }
 
-export default CustomApp;
+export default appWithTranslation(CustomApp, i18config);
