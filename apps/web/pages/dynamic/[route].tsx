39f215f8d07288709ad6466c6d8f1c957e0694e1
aiden <recovery@asleep.ai>
2022-06-04 17:11:43 +0900

    add dynamic routing i18n page

---
+++ apps/web/pages/dynamic/[route].tsx
@@ -0,0 +1,64 @@
+import { GetStaticPaths } from 'next';
+import { useTranslation } from 'next-i18next';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import i18config from '../../next-i18next.config';
+import styles from '../index.module.css';
+
+const DynamicRoute = () => {
+  const { t } = useTranslation('common');
+  const { locale, asPath } = useRouter();
+
+  return (
+    <div className={styles.warpper}>
+      <h1>{t('dynamic')}</h1>
+      <Link href={asPath} locale={locale === 'kr' ? 'en' : 'kr'}>
+        <a className={styles.button}>Change locale</a>
+      </Link>
+    </div>
+  );
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [
+      {
+        params: {
+          route: 'cat',
+        },
+        locale: 'kr',
+      },
+      {
+        params: {
+          route: 'cat',
+        },
+        locale: 'en',
+      },
+      {
+        params: {
+          route: 'dog',
+        },
+        locale: 'kr',
+      },
+      {
+        params: {
+          route: 'dog',
+        },
+        locale: 'en',
+      },
+    ],
+    fallback: false,
+  };
+};
+
+export async function getStaticProps({ locale }) {
+  console.log(await serverSideTranslations(locale, ['common'], i18config));
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ['common'], i18config)),
+    },
+  };
+}
+
+export default DynamicRoute;
--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,18 +1,18 @@
-import { SSRConfig, useTranslation } from 'next-i18next';
+import { useTranslation } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import i18config from '../next-i18next.config';
 import styles from './index.module.css';
 
-type IndexProps = SSRConfig;
-export function Index(props: IndexProps) {
-  const currentLocale = props._nextI18Next.initialLocale;
+export function Index() {
+  const { locale, asPath } = useRouter();
   const { t } = useTranslation('common');
 
   return (
     <div className={styles.warpper}>
       <h1>{t('title')}</h1>
-      <Link href="/" locale={currentLocale === 'kr' ? 'en' : 'kr'}>
+      <Link href={asPath} locale={locale === 'kr' ? 'en' : 'kr'}>
         <a className={styles.button}>Change locale</a>
       </Link>
     </div>
